1401829eeb7b628d23a331643bc5461eeab8c39d
Bren Yamaguchi <yamaguchibren@gmail.com>
2020-05-14 04:28:29 -0700

    fetch working in background and youtube data parsed

--- main.js
+++ main.js
@@ -6,17 +6,37 @@
                      &key={YOUR_API_KEY}
 */
 
-console.log(Url);
+const htmlString = document.documentElement.innerHTML;
+const htmlParsed = new HtmlParser(htmlString);
+const maxWord = htmlParsed.maxWord;
+const wordCache = htmlParsed.wordCache;
+console.log(wordCache);
+const ytKey = "AIzaSyC1zA9Yfx9Q6RSarv-JJdsY1F596oBxVLY";
 
-// let jsonText = fetch(Url)
-//   .then((response) => {
-//     console.log(response);
-//     return response.text();
-//   })
-//   .then((text) => {
-//     console.log(text);
-//     return text ? JSON.parse(text) : {};
-//   })
-//   .catch((error) => {
-//     console.error("Error:", error);
-//   });
+console.log(maxWord);
+
+// take maxWord and pass it to the youtube API
+
+// sending request message to background to handle API call.
+chrome.runtime.sendMessage({contentScriptQuery: 'queryYT', maxWord: maxWord, ytKey: ytKey}, parseYTData);
+
+// parse the youtube data when message comes back.
+function parseYTData(data) {
+  console.log(data);
+  const ytVidId = data.items[0].id.videoId;
+
+  // create a video tag node and store in a variable
+  let ytVideo = document.createElement("div");
+  ytVideo.setAttribute('id', 'yt-main-wrapper');
+  ytVideo.innerHTML = `<iframe id="youtube-discovery-video" src="https://www.youtube.com/embed/${ytVidId}?&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+  let ytWrapper = document.createElement('div');
+  ytWrapper.setAttribute('id', 'yt-maxword');
+  ytWrapper.textContent = `${maxWord}: ${wordCache[maxWord]}`;
+  
+  ytVideo.prepend(ytWrapper);
+  // find the body and store that into a variable
+  let body = document.querySelector("body");
+  // prepend video into the body
+  body.prepend(ytVideo);
+}
