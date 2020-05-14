55d4ea07edfc104f0a799cf3003d16d362e4930d
Bren Yamaguchi <yamaguchibren@gmail.com>
2020-05-14 00:08:08 -0700

    add static youtube in DOM

--- main.js
+++ main.js
@@ -3,9 +3,20 @@
 // instantiate a new instance of html parser with our html string passed in which will get
 // passed into our constructor
 // access maxWord from htmlParser instance
-// take maxWord and pass it to the youtube API
+
 
 let htmlString = document.documentElement.innerHTML;
 let htmlParsed = new HtmlParser(htmlString);
 console.log(htmlParsed.maxWord);
 // tests
+
+// take maxWord and pass it to the youtube API
+
+// take our html from htmlString
+// create a video tag node and store in a variable
+let ytVideo = document.createElement('div');
+    ytVideo.innerHTML = `<iframe id="youtube-discovery-video" src="https://www.youtube.com/embed/6yItCQB4V9A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+// find the body and store that into a variable
+let body = document.querySelector('body');
+// prepend video into the body
+body.prepend(ytVideo);
